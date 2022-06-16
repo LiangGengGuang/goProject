f2b2e1a003318a94cff77fb07b8dfb01a8a8761f
LiangGengguang <604046269@qq.com>
2022-06-16 22:00:33 +0800

    调整gin的cookie设置代码

--- src/ginWeb/7-cookie/main.go
+++ src/ginWeb/7-cookie/main.go
@@ -7,13 +7,13 @@ import (
 	models "project/5-models"
 )
 
-func setCookie(c *gin.Context) {
+func SetCookie(c *gin.Context) {
 	value := "lgg"
 	c.SetCookie("name", value, 3600, "/", "localhost", false, true)
 	c.JSON(http.StatusOK, models.SuccessResult(value))
 }
 
-func getCookie(c *gin.Context) {
+func GetCookie(c *gin.Context) {
 	cookie, err := c.Cookie("name")
 	if err != nil {
 		fmt.Println("cookie获取失败:", err)
@@ -23,7 +23,7 @@ func getCookie(c *gin.Context) {
 	c.JSON(http.StatusOK, models.SuccessResult(cookie))
 }
 
-func expireCookie(c *gin.Context) {
+func ExpireCookie(c *gin.Context) {
 
 	if _, err := c.Cookie("name"); err != nil {
 		fmt.Println("cookie不存在：", err)
@@ -40,15 +40,15 @@ func main() {
 	group := e.Group("/cookie")
 	{
 		group.GET("/setCookie", func(c *gin.Context) {
-			setCookie(c)
+			SetCookie(c)
 		})
 
 		group.GET("/getCookie", func(c *gin.Context) {
-			getCookie(c)
+			GetCookie(c)
 		})
 
 		group.GET("/expireCookie", func(c *gin.Context) {
-			expireCookie(c)
+			ExpireCookie(c)
 		})
 	}
 
