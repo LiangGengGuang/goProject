4764199c7106a6e04d6fcb14dd7af12e0dd1db2d
梁耿光 <604046268@qq.com>
2023-07-09 16:31:08 +0800

    调整代码目录

--- src/grpc/example/server/main.go
+++ src/grpc/example/server/main.go
@@ -4,18 +4,18 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
-	rpc2 "grpc/client/rpc"
+	"grpc/example/client/rpc"
 	"net"
 )
 
 // hello server
 
 type server struct {
-	rpc2.UnimplementedGreeterServer
+	rpc.UnimplementedGreeterServer
 }
 
-func (s *server) SayHello(ctx context.Context, in *rpc2.HelloRequest) (*rpc2.HelloResponse, error) {
-	return &rpc2.HelloResponse{Reply: "Hello " + in.Name}, nil
+func (s *server) SayHello(ctx context.Context, in *rpc.HelloRequest) (*rpc.HelloResponse, error) {
+	return &rpc.HelloResponse{Reply: "Hello " + in.Name}, nil
 }
 
 func main() {
@@ -25,8 +25,8 @@ func main() {
 		fmt.Printf("failed to listen: %v", err)
 		return
 	}
-	s := grpc.NewServer()                    // 创建gRPC服务器
-	rpc2.RegisterGreeterServer(s, &server{}) // 在gRPC服务端注册服务
+	s := grpc.NewServer()                   // 创建gRPC服务器
+	rpc.RegisterGreeterServer(s, &server{}) // 在gRPC服务端注册服务
 	// 启动服务
 	if s.Serve(lis); err != nil {
 		fmt.Printf("failed to serve: %v", err)
